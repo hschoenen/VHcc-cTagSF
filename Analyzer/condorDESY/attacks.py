feat: test DeepJet on PFNano and recalculate discriminators * fix: discriminators use all 6 output nodes instead of 4 * fix: model outputs need to be evaluated with Softmax function (probabilities between 0 and 1) * fix: names of sample, directory from args updated * fix: attacks now use available definitions for integers * fix: make model and criterion available for usage in FGSM style: improve readability (splitting lines, spacing, comments and printout) docs: remove outdated comments, leave only few old commands for comparison

Analyzer/condorDESY/attacks.py
@@ -18,7 +18,7 @@ def apply_noise(sample, magn=1e-2,offset=[0], dev=torch.device("cpu"), restrict_
 
         if var_group == 'glob':
             for i in range(vars_per_candidate['glob']):
-                if i in integer_per_variable[var_group]:
+                if i in integer_variables_by_candidate[var_group]:
                     xadv[:,i] = sample[:,i]
                 else: # non integer, but might have defaults that should be excluded from shift
                     defaults = sample[:,i].cpu() == defaults_per_variable[var_group][i]