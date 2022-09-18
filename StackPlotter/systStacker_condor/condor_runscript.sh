9850a78c7398bb92a4e60767c9382838720e3e8e
Annika Stein <annika-stein@t-online.de>
2022-09-18 23:31:53 +0200

    feat: plot stacked histograms via mplhep read root file, extract bin content (and error) and customize style

--- StackPlotter/systStacker_condor/condor_runscript.sh
+++ StackPlotter/systStacker_condor/condor_runscript.sh
@@ -1,4 +1,4 @@
-	OUTPUTDIR=/nfs/dust/cms/user/anstein/ctag_condor/systPlots_220917_2017_best_nominal_/Plots_220917_best_nominal_minimal_Custom_30bins_v4/
+	OUTPUTDIR=/nfs/dust/cms/user/anstein/ctag_condor/systPlots_220917_2017_best_adversarial_0p01_/Plots_220917_best_adversarial_0p01_minimal_Custom_30bins_v4/
 	OUTPUTNAME=output_2017_PFNano
 
 	CONDOR_CLUSTER_ID=$1
